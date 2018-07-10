5bf9cf0f3b5f41198ea9bce004ff0de7273e4f46
Robert Lindstaedt <robert.lindstaedt@gmail.com>
2018-07-10 21:07:14 +0200

    feat(v0): add transactions query

--- test/auth/login.spec.js
+++ test/auth/login.spec.js
@@ -22,11 +22,11 @@ test('v0.Auth: Can login with username password', function (t) {
   t.ok(auth)
 
   if (!process.env.SYSTEM_TEST) {
-    test.nock('https://staging-api.tillhub.com').post('/api/v0/users/login').reply(200, {
+    test.nock('https://api.tillhub.com').post('/api/v0/users/login').reply(200, {
       'status': 200,
       'msg': 'Authentication was good.',
       'request': {
-        'host': 'staging-api.tillhub.com',
+        'host': 'api.tillhub.com',
         'id': '11a12a8f-96bf-4380-aa55-301b7806f66f'
       },
       'user': {
@@ -41,15 +41,11 @@ test('v0.Auth: Can login with username password', function (t) {
     })
   }
 
-  auth.loginUsername(user.username, user.password, (err, body, resp) => {
+  auth.loginUsername(user.username, user.password, (err, { token, user }, resp) => {
     t.error(err)
 
-    t.ok(body)
-
-    t.ok(body.token)
-    t.ok(body.user)
-    t.ok(body.user.id)
-    t.equals(body.token_type, 'Bearer')
+    t.ok(token)
+    t.ok(user)
     t.end()
   })
 })
--- test/transactions/get.spec.js
+++ test/transactions/get.spec.js
@@ -15,21 +15,9 @@ if (process.env.SYSTEM_TEST) {
   user.password = process.env.SYSTEM_TEST_PASSWORD || user.password
 }
 
-test('root: Cannot init without credentials', function (t) {
+test('v0: Transactions: can fetch some transactions', function (t) {
   const Tillhub = require('../../lib/').Tillhub
-
-  const th = new Tillhub()
-
-  th.init((err) => {
-    t.ok(err)
-    t.equals(err.message, 'insufficient credentials')
-    t.equals(err.status, 422)
-    t.end()
-  })
-})
-
-test('root: Can init with auth credentials', function (t) {
-  const Tillhub = require('../../lib/').Tillhub
+  const Transactions = require('../../lib/v0/transactions').Transactions
 
   const th = new Tillhub({
     credentials: {
@@ -42,7 +30,7 @@ test('root: Can init with auth credentials', function (t) {
       'status': 200,
       'msg': 'Authentication was good.',
       'request': {
-        'host': 'staging-api.tillhub.com',
+        'host': 'api.tillhub.com',
         'id': '11a12a8f-96bf-4380-aa55-301b7806f66f'
       },
       'user': {
@@ -55,17 +43,35 @@ test('root: Can init with auth credentials', function (t) {
       'token_type': 'Bearer',
       'expires_at': '2018-06-05T11:11:27.000Z'
     })
+
+    test.nock('https://api.tillhub.com').get('/api/v0/transactions/anotheruuid/legacy').reply(200, {
+      'status': 200,
+      'msg': 'Fetched all transactions.',
+      'count': 0,
+      'results': [
+        {
+          '_id': 'someid'
+        }
+      ]
+    })
   }
 
   th.init((err, client, authInstance) => {
     t.error(err)
 
-    t.ok(th.auth)
-    t.ok(th.auth.token)
-    t.ok(th.auth.user)
-    t.ok(th.user)
-    t.ok(th.http)
-    t.ok(authInstance === th.auth)
-    t.end()
+    const transactions = th.transactions()
+    t.ok(transactions)
+    t.ok(transactions.http)
+    t.ok(transactions instanceof Transactions)
+
+    transactions.getAll((err, data, metadata) => {
+      t.error(err)
+
+      t.ok(Array.isArray(data))
+      t.ok(data.length)
+      t.ok(Number.isFinite(metadata.count))
+
+      t.end()
+    })
   })
 })
